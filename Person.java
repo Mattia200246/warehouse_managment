85ab20dbf9eb29ba91c3a52f6e26fe10d628c098
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2022-11-23 16:47:08 +0100

    created person class. the class rapresent a generic person

---
+++ Person.java
@@ -0,0 +1,16 @@
+/*
+ *  Rapresents generic Person
+ */
+public class Person {
+    final String name;
+    final String surname;
+    final String fiscalCode;
+    final String phone;
+
+    public Person(String name, String surname, String fiscalCode, String phone){
+        this.name = name;
+        this.surname = surname;
+        this.fiscalCode = fiscalCode;
+        this.phone = phone;
+    }
+}
