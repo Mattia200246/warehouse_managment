ab07eeb204f0ab0871c8a3aada1724b6d0b32af5
Mattia Flamigni <mattia.flamigni4@studio.unibo.it>
2022-11-23 17:00:06 +0100

    implemethed getter and toString

--- Person.java
+++ Person.java
@@ -15,4 +15,35 @@ public class Person {
         this.phone = phone;
         this.address = address;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getFiscalCode() {
+        return fiscalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", fiscalCode='" + fiscalCode + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
